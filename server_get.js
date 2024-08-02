8d95e4d9b78626801b5f2fb62004b9489235daf8
Your Name <s223882551@deakin.edu.au>
2024-08-02 15:39:02 +1000

    first commit

---
+++ server_get.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const express= require("express");
+const app= express();
+app.use(express.static('public'))
+// app.use(express.static(path.join(__dirname, 'public')))
+const addTwoNumber= (n1,n2)=>{
+    return n1+n2;
+}
+
+app.get("/addTwoNumber",(req,res)=>{
+    const n1= parseInt(req.query.n1);
+    const n2= parseInt(req.query.n2);
+    const result= addTwoNumber(n1,n2);
+    res.json({statuscode:200, data:result});
+});
+
+app.get("/Display",(req,res)=>{
+    const n1= "<html><body><H1>Hello There </H1></body></html>";
+    res.set('Content-Type','text/html');
+    res.send(Buffer.from(n1));
+})
+console.log(addTwoNumber(19,12));
+const port=3040;
+app.listen(port,()=>{
+    console.log("Hello im listening to port" +port);
+})
